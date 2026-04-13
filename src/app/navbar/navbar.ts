b3761b5d3aeba301d4ab116dfde2f42da97a6337
Mohit Singh <mohit2002pc@gmail.com>
2026-04-14 01:33:56 +0530

    dashboard with animation

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 
+import { MenubarModule } from 'primeng/menubar';
+import { SplitterModule } from 'primeng/splitter';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
+
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MenubarModule, SplitterModule, CardModule, ButtonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-
+  items = [
+  { label: 'ABOUT' },
+  { label: 'EDUCATION' },
+  { label: 'TECHNOLOGY & TOOLS' },
+  { label: 'EXPERIENCE' },
+  { label: 'CONTACT ME' }
+];
 }
