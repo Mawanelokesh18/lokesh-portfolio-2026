654672aa7c9b60424bba1bce613f8d04c3854acf
Lokesh Mawane <lokeshmawane@gmail.com>
2026-04-15 00:16:16 +0530

    change commit to me and update the dashboard of portfolio

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
+import { MenubarModule } from 'primeng/menubar';
+import { SplitterModule } from 'primeng/splitter';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MenubarModule, SplitterModule, CardModule, ButtonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+ isMobile = false;
+
+  ngOnInit() {
+    this.checkScreen();
+  }
+
+  @HostListener('window:resize')
+  checkScreen() {
+    this.isMobile = window.innerWidth < 900;
+  }
+
+  items = [
+    { label: 'ABOUT' },
+    { label: 'EDUCATION' },
+    { label: 'TECHNOLOGY & TOOLS' },
+    { label: 'EXPERIENCE' },
+    { label: 'CONTACT ME' }
+  ];
 
 }
